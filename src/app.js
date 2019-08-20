6c966d6c61eaab5f40526b5fa6c7eb049137f108
Aries <aries@asterism.xyz>
2019-08-20 15:39:37 +0900

    Send DM to user as find picture in the bot's HTL

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-// console.log('Hello, World')
 const Mastodon = require('megalodon').default;
 
 const BASE_URL = process.env.INSTANCE
-
 const access_token = process.env.ACCESS_TOKEN
+const target_account = process.env.TARGET_ACCOUNT
 
 const client = new Mastodon(access_token, BASE_URL + '/api/v1')
-
 const stream = client.stream('/streaming/user')
-// stream.on('connect', event => {
-//   console.log('connect')
-// })
 
 stream.on('update', status => {
-  console.log(status)
+  if(status.media_attachments.length != 0){
+    console.log(status.url)
+
+    client.post('/statuses', {
+      status: status.url,
+      visibility: "direct",
+      mention: {acct: target_account}
+    })
+  }
 })
