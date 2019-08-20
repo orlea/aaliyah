c01474fdaba8cfaaf8613f2ec3b81c41c5a3ee7e
Aries <aries@asterism.xyz>
2019-08-20 14:28:10 +0900

    using environment variable

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:10-alpine
 
-COPY package.json /src/package.json
-RUN cd /src; npm install --production
-COPY src /src/
-# CMD ["node", "src/app.js"]
+COPY package.json /package.json
+RUN npm install --production
+COPY src /src
+CMD ["node", "src/app.js"]
--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log('Hello, World')
+// console.log('Hello, World')
+const Mastodon = require('megalodon').default;
+
+const BASE_URL = process.env.INSTANCE
+
+const access_token = process.env.ACCESS_TOKEN
+
+const client = new Mastodon(access_token, BASE_URL + '/api/v1')
+
+const stream = client.stream('/streaming/user')
+// stream.on('connect', event => {
+//   console.log('connect')
+// })
+
+stream.on('update', status => {
+  console.log(status)
+})
